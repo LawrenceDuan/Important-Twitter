a3331478a567acdeb53926a8ecfd6c1ccbe7c6a7
Cheng Duan <lawrence.chengduan@gmail.com>
2018-04-03 17:24:23 +0100

    Create gitignore.

--- twitter_OAuth.py
+++ twitter_OAuth.py
@@ -1,11 +1,6 @@
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = 'NYICJJWGEHWjLzf16L4bdOmBp'
-consumer_secret = 'AT8vDFvB6IkjLM5Ckz9DC5yQ0nsqut8vwGYOpnXPMFIESsHKG5'
-access_token = '2909697444-NNDWfyczj7Asgv7t5YvysLcnnEj3CcoC12AB46R'
-access_secret = 'kRaSQf3FEEz3X7TSH1o0EisdweIGoxm9Af6E0WceFzeEp'
-
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
