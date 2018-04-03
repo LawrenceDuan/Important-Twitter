211e4e73c794602ffca7cd038ccc637ff0a61db8
Cheng Duan <lawrence.chengduan@gmail.com>
2018-04-03 17:09:09 +0100

    Twitter applicaiton config.

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'NYICJJWGEHWjLzf16L4bdOmBp'
+consumer_secret = 'AT8vDFvB6IkjLM5Ckz9DC5yQ0nsqut8vwGYOpnXPMFIESsHKG5'
+access_token = '2909697444-NNDWfyczj7Asgv7t5YvysLcnnEj3CcoC12AB46R'
+access_secret = 'kRaSQf3FEEz3X7TSH1o0EisdweIGoxm9Af6E0WceFzeEp'
